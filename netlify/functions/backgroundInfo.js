f41cbfcdd3457508225b25867ce0244c642dc3ad
SuperKIMario <mariowittmer@web.de>
2025-06-25 12:06:06 +0200

    Update backgroundInfo.js

--- netlify/functions/backgroundInfo.js
+++ netlify/functions/backgroundInfo.js
@@ -1,25 +1,26 @@
 // netlify/functions/backgroundInfo.js
-
-module.exports = {
-  profil: {
-    name: "Mario Wittmer",
-    rolle: "Vertriebsstratege | Coach | KI-gestützter Systemdenker",
-    positionierung: "Operativ. Strategisch. Automatisiert."
-  },
-  faecherschwerpunkte: [
-    "B2B-Vertrieb (Kalt- & Telefonakquise)",
-    "Leadgenerierung & Zielgruppenansprache",
-    "Vertriebspsychologie & Abschlussstärke",
-    "Coaching von Teams & Einzelpersonen",
-    "Automatisierung mit KI (GPT, Make, Airtable, Notion)",
-    "Funnel-Design & smarte Systemarchitektur",
-    "Chatbot-Entwicklung als Prozessbeschleuniger"
-  ],
-  softSkills: [
-    "Klar und strukturiert im Denken",
-    "Empathisch im Gespräch",
-    "Ziel- und lösungsorientiert",
-    "Autodidaktisch in KI-Themen",
-    "Systemisches Denken mit Menschenverstand"
-  ]
-};
+module.exports = [
+  {
+    profil: {
+      name: "Mario Wittmer",
+      rolle: "Vertriebsstratege | Coach | KI-gestützter Systemdenker",
+      positionierung: "Operativ. Strategisch. Automatisiert."
+    },
+    faecherschwerpunkte: [
+      "B2B-Vertrieb (Kalt- & Telefonakquise)",
+      "Leadgenerierung & Zielgruppenansprache",
+      "Vertriebspsychologie & Abschlussstärke",
+      "Coaching von Teams & Einzelpersonen",
+      "Automatisierung mit KI (GPT, Make, Airtable, Notion)",
+      "Funnel-Design & smarte Systemarchitektur",
+      "Chatbot-Entwicklung als Prozessbeschleuniger"
+    ],
+    softskills: [
+      "Klar und strukturiert im Denken",
+      "Empathisch im Gespräch",
+      "Ziel- und lösungsorientiert",
+      "Autodidaktisch in KI-Themen",
+      "Systemisches Denken mit Menschenverstand"
+    ]
+  }
+];
