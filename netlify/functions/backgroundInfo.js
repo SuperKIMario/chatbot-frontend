6b6b749d981df297fa91a58f26baf25d0d13595b
SuperKIMario <mariowittmer@web.de>
2025-06-25 10:07:05 +0200

    Create backgroundInfo.js

---
+++ netlify/functions/backgroundInfo.js
@@ -0,0 +1,21 @@
+// netlify/functions/backgroundInfo.js
+
+module.exports = {
+  profil: {
+    name: "Mario Wittmer",
+    rolle: "Vertriebsstratege | Coach | KI-gestützter Systemdenker",
+    positionierung: "Operativ. Strategisch. Automatisiert.",
+    // …die restlichen Felder aus deinem JSON…
+  },
+  faecherschwerpunkte: [
+    "B2B-Vertrieb",
+    "Leadgenerierung & Zielgruppenansprache",
+    // …
+  ],
+  softSkills: [
+    "Klar und strukturiert im Denken",
+    "Empathisch im Gespräch",
+    // …
+  ],
+  // etc.
+}
